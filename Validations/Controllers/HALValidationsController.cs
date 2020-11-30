2fb416ec4dc04121a466b6e042134ae9db594982
paragsarin <42388306+paragsarin@users.noreply.github.com>
2020-11-30 10:36:10 -0500

    Update HALValidationsController.cs

--- Validations/Controllers/HALValidationsController.cs
+++ Validations/Controllers/HALValidationsController.cs
@@ -29,7 +29,7 @@ namespace Validations.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("all Ok");
+            return Ok("Ok");
         }
         [HttpPost]
         public async Task<IActionResult> Post(ValidationRequest request)
